b056f745d6c4643cd7c4447104afb36bc85e9595
Michelle Burton <msmichelleburton@yahoo.com>
2025-02-16 16:05:35 -0600

    Refactor to just Node.js and terminal

--- js/app.js
+++ js/app.js
@@ -1,74 +1,32 @@
- function userInput(){
-        var startVal = prompt('Please enter start value', "1");
-        var validVal = validateMe(startVal);
-        if(startVal != null){
-            document.getElementById("title").innerHTML="Start Value: " + startVal;
-        insertStartVal(validVal);
-        };
-    };
+// Terminal application with Javascript Node.js
+const readline = require('readline');
 
-    function errorPrompt(){
-         var error = alert("Invalid Input, please try again");
-            userInput();
-            // Do I need to do a return?
-            // is this bad code?
-            // Do I need to create a error variable to pop
-            // alert box?
-            // seems like it should be able to return and //call next function.
-    }
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    function validateMe (startVal){
-        var validString=""
-        if (typeof startVal === "string"){
-             //console.log("function before conversion: " + typeof(startVal) );
-             // Convert "22" to int 22;
-             validString=+ startVal;
+function fizzBuzz(limit) {
+    for (let i = 1; i <= limit; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
         }
-        if (isNaN(validString)){
-           errorPrompt();
-        };
-
-        return validString;
     }
+}
 
+// Example usage:
+rl.question('Enter a number: ', (answer) => {
+    const n = parseInt(answer.trim(), 10);
+    fizzBuzz(n);
+    rl.close();
+});
 
-    function mod3(i) {
-        return ((i % 3) === 0) ? "Fizz" : false;
-    }
-
-    function mod5(i) {
-        return ((i % 5) === 0) ? "Buzz" : false;
-    }
-
-    function mod3and5(i) {
-        return (((i % 3) === 0) && ((i % 5) === 0)) ? "Fizz-Buzz" : false;
-    }
-
- function insertStartVal(startVal){
-        var arrLength = startVal;
-        var count = new Array();
-        var textInsert = "";
-
-
-    for (var i = 1; i < arrLength; i++) {
-        count[i] = i;
-        var answer3 = mod3(count[i]);
-        var answer5 = mod5(count[i]);
-        var answer3and5 = mod3and5(count[i])
-
-        if (answer3) {
-            count[i] = answer3;
-        };
-        if (answer5) {
-            count[i] = answer5;
-        };
-        if (answer3and5) {
-            count[i] = answer3and5;
-        };
-
-        textInsert += "<div>" + count[i] + "</div>";
-    }
-
-    $("body").append(textInsert);
-
- };
+// Example usage:
+const n = parseInt(readLine().trim(), 10);
+fizzBuzz(n);
